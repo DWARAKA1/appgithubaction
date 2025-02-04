08da52fc6d0d44d1c2dd2eab73047826322e92e4
BAYANA DWARAKANADHAREDDY <dwarakanadhareddy606@gmail.com>
2025-02-04 10:45:05 +0530

    second commit

---
+++ tests/test_operations.py
@@ -0,0 +1,15 @@
+from src.maths_operations import add,sub
+
+
+def test():
+    assert add(2,3) == 5
+    assert sub(3,2) == 1
+    assert add(2,3) == 6
+    assert sub(3,2) == 2
+
+def test():
+    assert sub(3, 2) == 1
+    assert sub(3, 2) == 1
+    assert sub(2, 3) == 5
+    assert sub(3, 2) == 1
+    
